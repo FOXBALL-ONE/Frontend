0a029cd0318520533df99bf392230cd79e5e5252
FOXBALL-ONE <admin@foxball.dev>
2025-07-25 13:09:10 +0800

    增加密码修改的二次验证

--- src/page/User/ChangePassword.vue
+++ src/page/User/ChangePassword.vue
@@ -8,8 +8,16 @@
       </div>
       <div>
         新密码
-
-        <a-input-password v-model:value="newPassword" placeholder="input password">
+        <a-input-password v-model:value="newPassword1" placeholder="input password">
+          <template #iconRender="v">
+            <EyeTwoTone v-if="v"></EyeTwoTone>
+            <EyeInvisibleOutlined v-else></EyeInvisibleOutlined>
+          </template>
+        </a-input-password>
+      </div>
+      <div>
+        确认密码
+        <a-input-password v-model:value="newPassword2" placeholder="input password">
           <template #iconRender="v">
             <EyeTwoTone v-if="v"></EyeTwoTone>
             <EyeInvisibleOutlined v-else></EyeInvisibleOutlined>
@@ -27,13 +35,18 @@ import {EyeInvisibleOutlined, EyeTwoTone} from '@ant-design/icons-vue';
 import {message} from "ant-design-vue";
 
 const oldPassword = ref<string>('');
-const newPassword = ref<string>('');
+const newPassword1 = ref<string>('');
+const newPassword2 = ref<string>('');
 
 const handleSubmit = () => {
-  if (oldPassword.value.length > 0 && newPassword.value.length > 0) {
+  if (oldPassword.value.length > 0 && newPassword1.value.length > 0 && newPassword2.value.length > 0) {
+    if (newPassword1.value !== newPassword2.value) {
+      message.error('两次输入的新密码不一致，请重新输入');
+      return;
+    }
     message.success('密码修改成功');
     message.info(oldPassword.value);
-    message.info(newPassword.value);
+    message.info(newPassword1.value);
     return;
   }
 
