7172f8bb6a1af363a81e55a17cca6157f9f29567
FOXBALL-ONE <admin@foxball.dev>
2025-07-28 10:15:28 +0800

    搜索框居中设计

--- src/page/Bounty/BountySearch.vue
+++ src/page/Bounty/BountySearch.vue
@@ -1,19 +1,23 @@
 <template>
-  <a-space direction="vertical">
-    <a-input-search
-        v-model:value="value"
-        :bordered="true"
-        enter-button
-        :maxlength="10"
-        placeholder="input search text"
-        @search="onSearch"
-    />
-  </a-space>
-  <!--  搜索内容展示-->
-  <a-space v-if="isSearched && value" direction="vertical">
-    <h1>123123123123123</h1>
-  </a-space>
+  <div class="search-container">
+    <a-space class="search-wrapper" direction="vertical">
+      <a-input-search
+          v-model:value="value"
+          :bordered="true"
+          :maxlength="10"
+          class="search-input"
+          enter-button
+          placeholder="input search text"
+          @search="onSearch"
+      />
+    </a-space>
+    <!--  搜索内容展示-->
+    <a-space v-if="isSearched && value" direction="vertical">
+      <h1>123123123123123</h1>
+    </a-space>
+  </div>
 </template>
+
 <script lang="ts" setup>
 import {ref} from 'vue';
 import {message} from "ant-design-vue";
@@ -28,6 +32,23 @@ const onSearch = (searchValue: string) => {
   message.success(value.value);
   isSearched.value = true;
 };
-
 </script>
 
+<style scoped>
+.search-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+
+.search-wrapper {
+  width: 100%;
+  max-width: 500px; /* 可以根据需要调整搜索框的最大宽度 */
+  margin: 0 auto;
+}
+
+.search-input {
+  width: 100%;
+}
+</style>
